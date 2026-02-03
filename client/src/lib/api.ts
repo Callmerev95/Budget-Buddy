c35c8e4c5fa09dd3906b0fcd82bc2f3fdec7bfae
Rev <rchivas109@gmail.com>
2026-02-03 16:35:38 +0800

    fix: update API base URL to production railway

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Alamat server backend kita
+  baseURL: "https://budget-buddy-production-523b.up.railway.app/", // Alamat server backend
   headers: {
     "Content-Type": "application/json",
   },
